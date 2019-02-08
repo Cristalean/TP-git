82ec9883e898570d5e18a466b0ce397f2758ec98
Axel LEFORT <axel.lefort@hotmail.fr>
2019-02-08 11:33:35 +0100

    display copyright

--- src/application.bash
+++ src/application.bash
@@ -1,3 +1,4 @@
- ### Application.bash - 201x - Axel Lefort ###
-echo "version 1"
+### Application.bash - 201x - Axel Lefort ###
+echo "version 1.1"
+echo "Copyright Money.inc - 201x"
 echo "Build : 10:00"
